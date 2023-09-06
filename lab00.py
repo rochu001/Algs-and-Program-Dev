33f082923d3128d86da116bfafa0a17f2ddb28c4
rochu001 <144243856+rochu001@users.noreply.github.com>
2023-09-06 08:17:40 -0500

    Add files via upload

---
+++ lab00.py
@@ -0,0 +1,36 @@
+
+import platform
+
+vers = platform.python_version()
+assert vers[0] == '3', "You must use Python 3, "+vers+" is not acceptable"
+print("Python 3 confirmed.")
+
+
+# num_cookies = input("How many cookies do you want to make? ")
+# recipe_mult = 12/num_cookies
+# butter = str(125*recipe_mult)+"g butter"
+# sugar = 225*recipe_mult+"g sugar"
+# eggs = str(max(1,round(recipe_mult))+" eggs"
+# vanilla = str(recipe_mult)+" tsp vanilla extract"
+# flour = str(225*recipe_mult)+g flour
+# salt = str(0.5*recipe_mult)+" tsp salt"
+# str(200*recipe_mult)+"g chocolate chips" = chips
+# print(butter)
+# print("sugar")
+# print(eggs)
+# print(vanilla)
+# print(flour)
+# print(salt)
+# print(chips)
+
+
+# amt = float(input("Enter the loan amount in dollars: ")
+# N = int(input("Enter the loan duration in months: "))
+# r = input("Enter the % annual interest rate: ")/100/12
+# payment = r*amt/1 - (1+r)**(-n)
+# print("Your monthly payment is: " + round(payment,2))
+
+
+# leave these lines commented out, this isn't Python code
+# Name of the TA who you visited in office hours:
+# Above TA's favorite color: 
